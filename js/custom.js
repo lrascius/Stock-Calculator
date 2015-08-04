33985355f4bdddd7900dae8adedd02a9cd097cd8
Lukas Rascius <lrascius93@gmail.com>
2015-08-03 22:21:22 -0400

    Adding profit calculation

--- js/custom.js
+++ js/custom.js
@@ -11,8 +11,10 @@ function calculate()
     /* Profit Calculations */
     var initialprice = $("#initialprice").val();
     var finalprice = $("#finalprice").val();
+    var shares = $("#shares").val();
     var profit_percent = ((finalprice - initialprice) / initialprice) * 100;
     if(isNaN(profit_percent)) 
         profit_percent = 0;
-    jQuery("label[for='profit_percent']").html(profit_percent + " %");
+    jQuery("label[for='profit_percent']").html("Percent Profit: " + profit_percent + " %");
+    jQuery("label[for='profit_dollars']").html("Profit: " + (profit_percent * shares * initialprice) + " $");
 }
