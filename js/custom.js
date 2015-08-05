ec5875842eb797c618f7a625779c71ae9ac66c57
Lukas Rascius <lrascius93@gmail.com>
2015-08-04 21:36:42 -0400

    Adding some functionality

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,71 @@
+/*global $, jQuery */
+
 $(document).ready(function () {
-    calculate();
+    calculateProfit();
+    calculateEarnings();
 });
 
 $('#responsiveTabsDemo').responsiveTabs({
     startCollapsed: 'accordion'
 });
 
-function calculate()
+function calculateProfit()
 {
     /* Profit Calculations */
+    
+    /* Get the input values */
+    var allotment = $('#allotment').val();
     var initialprice = $("#initialprice").val();
+    var buycommision = $('#buycommision').val();
     var finalprice = $("#finalprice").val();
-    var shares = $("#shares").val();
+    var sellcommision = $('#sellcommision').val();
+//    console.log("Allotment" + allotment);
+//    console.log("Initial price " + initialprice);
+//    console.log("Buy commision " + buycommision);
+//    console.log("Final price " + finalprice);
+//    console.log("Sell commision " + sellcommision);
+    
+    /* Calculate the profits */
+//    var profit_percent = percent_profit();
+    var netprofit = net_profit(initialprice, finalprice, buycommision, sellcommision, allotment);
+    console.log("Net " + netprofit);
+    
+    /* Add the results to the labels */
+    //    jQuery("label[for='profit_percent']").html("Return on Investment: " + percent_profit(initialprice, finalprice) + " %");
+    jQuery("label[for='marketvalue']").html("Market Value: $" + allotment * finalprice);
+    jQuery("label[for='cost']").html("Cost: $" + (allotment * initialprice + parseInt(buycommision) + parseInt(sellcommision)));
+    jQuery("label[for='netprofit']").html("Return on Investment: $" + netprofit);
+    jQuery("label[for='returnoninvestment']").html("Return on Investment: $" + netprofit);
+}
+
+function calculateEarnings()
+{
+    /* Earning Calculations */
+    
+    /* Get the input values */
+    var allotment = $('#allotment2').val();
+    var initialprice = $("#initialprice2").val();
+    var buycommision = $('#buycommision2').val();
+    var price = $("#price").val();
+    var number = $("#number").val();
+    
+    
+    console.log("Allotment" + allotment);
+    console.log("Initial price " + initialprice);
+    console.log("Buy commision " + buycommision);
+    console.log("Price " + price);
+    console.log("Number " + number);
+}
+
+function net_profit(initialprice, finalprice, buycommision, sellcommision, allotment)
+{
+    return (allotment * (finalprice - initialprice)) - sellcommision - buycommision;
+}
+
+function percent_profit(initialprice, finalprice)
+{
     var profit_percent = ((finalprice - initialprice) / initialprice) * 100;
     if(isNaN(profit_percent)) 
         profit_percent = 0;
-    jQuery("label[for='profit_percent']").html("Percent Profit: " + profit_percent + " %");
-    jQuery("label[for='profit_dollars']").html("Profit: " + (profit_percent * shares * initialprice) + " $");
+    return profit_percent;
 }
