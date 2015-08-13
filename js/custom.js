b9133596339b4961781d0158fe1050b16eca20db
Lukas Rascius <lrascius93@gmail.com>
2015-08-12 20:10:09 -0400

    Fixing some stuff and adding profit/loss calculation functionality

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function () {
     calculateProfit();
-    calculateEarnings();
+//    calculateEarnings();
 });
 
 $('#responsiveTabsDemo').responsiveTabs({
@@ -26,35 +26,63 @@ function calculateProfit()
 //    console.log("Sell commision " + sellcommision);
     
     /* Calculate the profits */
-//    var profit_percent = percent_profit();
     var netprofit = net_profit(initialprice, finalprice, buycommision, sellcommision, allotment);
-    console.log("Net " + netprofit);
+    var cost = allotment * initialprice + parseFloat(buycommision) + parseFloat(sellcommision);
+    var roi = (netprofit / cost) * 100;
+    if(isNaN(roi))
+        roi = 0;
     
     /* Add the results to the labels */
-    //    jQuery("label[for='profit_percent']").html("Return on Investment: " + percent_profit(initialprice, finalprice) + " %");
-    jQuery("label[for='marketvalue']").html("Market Value: $" + allotment * finalprice);
-    jQuery("label[for='cost']").html("Cost: $" + (allotment * initialprice + parseInt(buycommision) + parseInt(sellcommision)));
-    jQuery("label[for='netprofit']").html("Return on Investment: $" + netprofit);
-    jQuery("label[for='returnoninvestment']").html("Return on Investment: $" + netprofit);
+    if(allotment != 0)
+    {
+        jQuery("label[for='marketvalue']").html(accounting.formatMoney(allotment * finalprice));
+        jQuery("label[for='cost']").html(accounting.formatMoney(cost));
+        jQuery("label[for='netprofit']").html(accounting.formatMoney(netprofit));
+        jQuery("label[for='returnoninvestment']").html(roi.toFixed(2) + " %");
+        if(netprofit < 0)
+        {
+            jQuery("label[for='netprofit']").css("color", "red");
+            jQuery("label[for='returnoninvestment']").css("color", "red");
+        }
+        if(netprofit > 0)
+        {
+            jQuery("label[for='netprofit']").css("color", "green");
+            jQuery("label[for='returnoninvestment']").css("color", "green");
+        }
+    }
+    else 
+    {
+        jQuery("label[for='marketvalue']").html(accounting.formatMoney(0));
+        jQuery("label[for='cost']").html(accounting.formatMoney(0));
+        jQuery("label[for='netprofit']").html(accounting.formatMoney(0));
+        jQuery("label[for='returnoninvestment']").html("0.00 %");
+        jQuery("label[for='netprofit']").css("color", "black");
+        jQuery("label[for='returnoninvestment']").css("color", "black");
+    }
 }
 
 function calculateEarnings()
 {
+    /* Remove the previous price labels */
+    $( ".pricelabels" ).remove();
     /* Earning Calculations */
     
     /* Get the input values */
     var allotment = $('#allotment2').val();
     var initialprice = $("#initialprice2").val();
     var buycommision = $('#buycommision2').val();
+    var sellcommision = $('#sellcommision2').val();
     var price = $("#price").val();
     var number = $("#number").val();
     
+    var cost = allotment * initialprice + parseFloat(buycommision) + parseFloat(sellcommision);
     
-    console.log("Allotment" + allotment);
-    console.log("Initial price " + initialprice);
-    console.log("Buy commision " + buycommision);
-    console.log("Price " + price);
-    console.log("Number " + number);
+    for(var i = 1; i <= number; i++)
+    {
+        var marketvalue = i*price + cost;
+        var $label = $("<label class='pricelabels'>").text("Profit of " + accounting.formatMoney(i*price) + ": Sell at " + accounting.formatMoney(marketvalue / allotment));
+        $('#earningstab').append($label);
+    }
 }
 
 function net_profit(initialprice, finalprice, buycommision, sellcommision, allotment)
@@ -62,10 +90,3 @@ function net_profit(initialprice, finalprice, buycommision, sellcommision, allot
     return (allotment * (finalprice - initialprice)) - sellcommision - buycommision;
 }
 
-function percent_profit(initialprice, finalprice)
-{
-    var profit_percent = ((finalprice - initialprice) / initialprice) * 100;
-    if(isNaN(profit_percent)) 
-        profit_percent = 0;
-    return profit_percent;
-}
